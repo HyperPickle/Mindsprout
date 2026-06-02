c745d8c609977219a52b4e60392804e6605bfe23
Rishi Singhal <rishiaussie@gmail.com>
2026-06-03 09:43:50 +1000

    Initial Commit

---
+++ Mindsprout/MindsproutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindsproutApp.swift
+//  Mindsprout
+//
+//  Created by Rishi Singhal on 3/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct MindsproutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
