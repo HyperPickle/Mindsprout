f90c5be31f49d810ec5bfcd6fe369f36ee9add89
Rishi Singhal <rishiaussie@gmail.com>
2026-06-04 23:01:37 +1000

    Refactor onboarding sign view to use shared flow

--- Mindsprout/Features/Onboarding/SignView.swift
+++ Mindsprout/Features/Onboarding/SignView.swift
@@ -1,37 +1,15 @@
-//
-//  SignView.swift
-//  MindSprout
-//
-//  Created by Changrila Souksamlane on 3/6/2026.
-//
-
 import SwiftUI
 
 struct SignView: View {
-    @EnvironmentObject var navVM: NavigationViewModel
-    @State var Email = ""
-    @State var Password = ""
-    @State var Remember = false
-    var body: some View {
-        ZStack{
-            BackgroundSky()
-            
-            ScrollView(.vertical, showsIndicators: false){
-                if navVM.showSignUp {
-                    SignUp(Email: $Email, Password: $Password, Remember: $Remember, showSignUp: $navVM.showSignUp, action:{})
-                        .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .trailing)))
-                } else {
-                    SignIn(Email: $Email, Password: $Password, Remember: $Remember, showSignUp: $navVM.showSignUp, action:{})
-                        .transition(.asymmetric(insertion: .move(edge: .leading), removal: .move(edge: .leading)))
-                }
-            }
-            
-        }
+    @Environment(\.dismiss) private var dismiss
 
+    var body: some View {
+        OnboardingView {
+            dismiss()
+        }
     }
 }
 
 #Preview {
     SignView()
-        .environmentObject(NavigationViewModel())
 }
